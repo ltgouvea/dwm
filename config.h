0031c463824756c0a8bdccbf1e60d6ce574a2253
Lucas Gouvêa (K2) <lucas.fim@gmail.com>
2019-10-23 20:39:02 -0300

    icons for workspaces and some rules

--- config.h
+++ config.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "SauceCodePro Nerd Font Mono:size=12" };
-static const char dmenufont[]       = "SauceCodePro Nerd Font Mono:size=12";
+static const char *fonts[]          = { "SauceCodePro Nerd Font Mono:size=15" };
+static const char dmenufont[]       = "SauceCodePro Nerd Font Mono:size=15";
 
 
 /* pywal generated colorscheme */
@@ -22,7 +22,7 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "", "", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -30,8 +30,9 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Firefox",  NULL,       NULL,         2,            0,           -1 },
+	{ "Spotify",  NULL,       NULL,         7,            0,           -1 },
+	{ "Steam",    NULL,       NULL,         4,            1,           -1 }
 };
 
 /* layout(s) */
